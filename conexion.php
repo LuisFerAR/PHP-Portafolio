41a2db84daa25dd0850fdffc090165eb7bb0bdd9
Luis Fernando Aranda Ramos <129498249+LuisFerAR@users.noreply.github.com>
2023-12-12 11:11:26 -0500

    g

--- conexion.php
+++ conexion.php
@@ -6,20 +6,27 @@ class conexion{
     private $usuario="root";
     private $contrasenia="";
     private $conexion;
-
+    //CREACION DE LA CONEXION CON EL CONSTRUCTOR
     public function __construct(){
     try{
             $this->conexion=new PDO("mysql:host=$this->servidor;dbname=album", $this->usuario, $this->contrasenia);
             $this->conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             }catch( PDOException $e){
-            return "falla de connexion".$e;
+            return "falla de conexion".$e;
         }
     }
 
-    public function ejecutar($sql){
+    public function ejecutar($sql){ //Insertar, delete, actualizar
         $this->conexion->exec($sql);
         return $this->conexion->lastInsertId();
+    }
+    //SE CONSULTA LA INFORMACION
+    public function consultar($sql){
+        $sentencia=$this->conexion->prepare($sql);
+        $sentencia->execute();
+        return $sentencia->fetchAll();
+
     }
    
 }
--- portafolio.php
+++ portafolio.php
@@ -3,13 +3,33 @@
 <?php 
 
     if($_POST){
-
         print_r($_POST);
         $nombre=$_POST['nombre'];
-    $objConexion=new conexion();
-    $sql="INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', 'imagen.jpg', 'proyecto de hace mucho tiempo')";
-    $objConexion->ejecutar($sql);
+        $imagen=$_FILES['archivo']['name'];
+        $descripcion=$_POST['descripcion'];
+        $imagen_temporal=$_FILES['archivo']['tmp_name'];
+
+        move_uploaded_file($imagen_temporal, "imagenes/".$imagen);
+
+        $objConexion=new conexion();
+        $sql="INSERT INTO `proyectos` (`id`, `nombre`, `imagen`, `descripcion`) VALUES (NULL, '$nombre', '$imagen', '$descripcion')";
+        $objConexion->ejecutar($sql);
+
     }
+    if($_GET){
+        //DELETE FROM `proyectos` WHERE `proyectos`.`id` = 9
+        $objConexion=new conexion();
+        $id=$_GET['borrar'];
+        $sql="DELETE FROM `proyectos` WHERE `proyectos`.`id`=".$id;
+        $objConexion->ejecutar($sql);
+    }
+
+
+    //INSTANCIAR LOS DATOS Y SE EJECUTA CON EL METODO CONSULTAR
+    $objConexion=new conexion();
+    $proyectos=$objConexion->consultar("SELECT * FROM `proyectos`");
+   // print_r($proyectos);
+
 ?>
    
    <br>
@@ -22,6 +42,7 @@
                             <form action="portafolio.php" method="post" enctype="multipart/form-data">
                                 Nombre del proyecto: <input class="form-control" type="text" name="nombre" id=""><br/>
                                 Imagen del proyecto: <input class="form-control" type="file" name="archivo" id=""><br/>
+                                Descripcion: <textarea class="form-control" name="descripcion" id="" rows="3" ></textarea><br>
                                 <input class="btn btn-success" type="submit" value="Enviar proyecto">
                             </form>
                         </div>
@@ -34,15 +55,20 @@
                     <tr>
                         <th>ID</th>
                         <th></th>Nombre</th>
-                        <th>Imagen</the=>
+                        <th>Imagen</th>
+                        <th>Descripcion</th>
                     </tr>
                 </thead>
                 <tbody>
+                <?php foreach($proyectos as $proyecto){  ?>
                     <tr>
-                        <td>3</td>
-                        <td>Aplicacion web</td>
-                        <td>Imagen.jpg</td>
+                        <td><?php echo $proyecto['id']; ?></td>
+                        <td><?php echo $proyecto['nombre']; ?></td>
+                        <td><?php echo $proyecto['imagen']; ?></td>
+                        <td><?php echo $proyecto['descripcion']; ?></td>
+                        <td> <a name="" id="" class="btn btn-primary" href="?borrar=<?php echo $proyecto['id']; ?>" role="button">Eliminar</a></td>
                     </tr>
+                    <?php } ?>
                 </tbody>
             </table>
             </div>
